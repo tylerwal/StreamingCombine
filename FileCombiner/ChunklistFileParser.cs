fd44cb144a5695d609e6dfdf5bf68bcc5a40b784
Tyler Waldron <tylerwal@gmail.com>
2015-01-02 22:21:11 -0500

    Adding initial working application that can download the list of uris from the m3u8 fle

---
+++ FileCombiner/ChunklistFileParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace FileCombiner
+{
+	public class ChunklistFileParser
+	{
+		private const char NewLineCharacter = '\n';
+		
+		public Uri BaseAddress
+		{
+			get;
+			set;
+		}
+
+		public Queue<Uri> StreamingFileUris
+		{
+			get;
+			set;
+		}
+
+		public ChunklistFileParser(FileInfo inputFile, string baseAddress)
+		{
+			BaseAddress = new Uri(baseAddress);
+
+			var allFileLines = File.ReadLines(inputFile.FullName);
+
+			ProcessUnparsedFileIntoUris(allFileLines);
+		}
+
+		public ChunklistFileParser(string chunkListUrl)
+		{
+			int lastSlashLocation = chunkListUrl.LastIndexOf('/');
+			string baseAddress = chunkListUrl.Remove(lastSlashLocation + 1);
+
+			BaseAddress = new Uri(baseAddress);
+
+			WebClient webClient = new WebClient();
+
+			var allFileLines = webClient.DownloadString(chunkListUrl).Split('\n');
+			
+			ProcessUnparsedFileIntoUris(allFileLines);
+		}
+
+		#region Helper Methods
+
+		private void ProcessUnparsedFileIntoUris(IEnumerable<string> allFileLines)
+		{
+			var streamingFileLines = ParseStreamFiles(allFileLines);
+
+			StreamingFileUris = GetStreamFileUris(streamingFileLines);
+		}
+
+		private Queue<Uri> GetStreamFileUris(IEnumerable<string> streamingFileLines)
+		{
+			Queue<Uri> orderedStreamFileUris = new Queue<Uri>();
+
+			foreach (string streamingFileLine in streamingFileLines)
+			{
+				string trimmedLine = streamingFileLine.Trim();
+
+				if (string.IsNullOrWhiteSpace(trimmedLine))
+				{
+					continue;
+				}
+
+				Uri combinedUri = new Uri(BaseAddress, trimmedLine);
+
+				orderedStreamFileUris.Enqueue(combinedUri);
+			}
+
+			return orderedStreamFileUris;
+		}
+		
+		/// <summary>
+		/// Removes the metadata in M3U files.
+		/// Files that start with #EXT are not relevant.
+		/// </summary>
+		/// <param name="unparsedLines">Collection of unparsed lines that could contain metadata as well as streaming file urls.</param>
+		/// <returns>Just the streaming file url lines.</returns>
+		private IEnumerable<string> ParseStreamFiles(IEnumerable<string> unparsedLines)
+		{
+			return unparsedLines.Where(i => !i.StartsWith("#EXT"));
+		}
+
+		#endregion Helper Methods
+	}
+}
---
+++ FileCombiner/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCombiner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string chunkListFilePath = @"http://media.strava.com/vod/smil:TheScream.smil/chunklist_w2086349083_b2400000.m3u8?timestamp=1420335686&stream=TheScream&signature=bed48b215c9ddf4653503e9f5ff8c77c467ecd4f00132d732f6fb6af58497968";
+
+			ChunklistFileParser chunklistFileParser = new ChunklistFileParser(chunkListFilePath);
+
+			string outputDirectory = @"C:\Programming\TheLongScream\";
+			string outputFilePrepend = "TheLongScream";
+			string outputFileExtension = ".ts";
+
+			if (!Directory.Exists(outputDirectory))
+			{
+				Directory.CreateDirectory(outputDirectory);
+			}
+
+			WebClient webClient = new WebClient();
+
+			int numberOfUris = chunklistFileParser.StreamingFileUris.Count;
+			
+			for (int i = 0; i < numberOfUris; i++)
+			{
+				Console.WriteLine("Downloading File #{0}", i);
+
+				var currentUri = chunklistFileParser.StreamingFileUris.Dequeue();
+
+				StringBuilder pathBuilder = new StringBuilder();
+				pathBuilder.Append(outputDirectory);
+				pathBuilder.Append(outputFilePrepend);
+				pathBuilder.Append('_');
+				pathBuilder.Append(i);
+				pathBuilder.Append(outputFileExtension);
+
+				byte[] data = webClient.DownloadData(currentUri);
+
+				File.WriteAllBytes(pathBuilder.ToString(), data);
+			}
+		}
+	}
+}
